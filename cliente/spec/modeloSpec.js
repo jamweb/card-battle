7e5a9886461bb1ece89ced2d268ac6db84e25fc3
JOSE ANGEL MARTINEZ MARTINEZ <JoseAngel.Martinez5@alu.uclm.es>
2018-10-01 13:31:40 +0200

    Reestructuracion del proyecto y pruebas ok

---
+++ cliente/spec/modeloSpec.js
@@ -0,0 +1,53 @@
+describe("El juego de las cartas...", function() {
+  var juego;
+  var usr;
+  //var cartalobo;
+
+  beforeEach(function() {
+    juego = new Juego();
+    usr = new Usuario("pepe")
+   // cartalobo= new Carta("lobo",9,9,9);
+  });
+
+  it("deberia tener una coleccion de cartas", function() {
+    
+    expect(juego.cartas).toBeDefined();
+    expect(juego.cartas.length).toEqual(30);
+    expect(juego.usuarios).toBeDefined();
+    expect(juego.usuarios.length).toEqual(0);
+
+  });
+
+  it("el usuario tiene un mazo", function() {
+    
+    expect(usr.mazo).toBeDefined();
+    expect(usr.mazo.length).toEqual(0);
+  });
+
+  it("Agrego el usuario pepe al juego", function() {
+    
+    juego.agregarUsuario(usr);
+    expect(juego.usuarios.length).toEqual(1);
+    expect(juego.usuarios[0].nombre).toEqual("pepe");
+    expect(usr.mazo.length).toEqual(30);   
+  })
+
+  /*it("El usuario tiene una mano vacia inicialmente", function() {
+    
+    juego.agregarUsuario(usr);
+    expect(juego.usuarios.length).toEqual(1);
+    expect(juego.usuarios[0].mano).toEqual(0);
+    expect(usr.mazo.length).toEqual(30);   
+  })*/
+
+  /*
+  it("Agrego carta lobo al juego", function() {
+    
+    juego.agregarCarta(cartalobo);
+    expect(juego.cartas.length).toEqual(31);
+    expect(juego.cartas[30].nombre).toEqual("lobo");
+    expect(cartalobo.vidas).toEqual(9);   
+  })
+  */
+
+}); 
---
+++ index.js
@@ -0,0 +1,45 @@
+var fs=require("fs");
+var config=JSON.parse(fs.readFileSync("config.json"));
+var host=config.host;
+var port=config.port;
+var exp=require("express");
+var app=exp(); 
+
+var modelo = require("./servidor/modelo.js"); 
+var juego = new modelo.Juego(); //cuando se inicia el servidor se crea una instancia de juego
+
+//app.use(app.router);
+//app.use(exp.static(__dirname + "/public"));
+
+
+app.get("/",function(request,response){
+	var json={};
+	response.send(json);
+});
+/*
+app.get("/hola/:text",function(request,response){
+	response.send("Hola "+request.params.text);
+});
+var users ={
+	"1":{
+		"name":"Pepe Lopez",
+		"cuenta":"@pepe"
+	},
+	"2":{
+		"name":"Juan Perez",
+		"cuenta":"@juan"
+	}
+}
+app.get("/user/:id",function(request,response){
+	var user=users[request.params.id];
+	if(user){
+		response.send("Usuario: "+user.name+" cuenta: "+user.cuenta);
+	}
+	else{
+		response.send("usuario no existe");
+	}
+	response.send
+})
+*/
+console.log("Servidor escuchando en "+host+":"+port);
+app.listen(port,host);
